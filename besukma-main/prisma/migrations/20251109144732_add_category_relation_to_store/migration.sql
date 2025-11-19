786783c3647306c8402c65023f5b10dc209c61ca
Yashin Al Fauzy <yashinfauzy@apps.ipb.ac.id>
2025-11-19 13:25:16 +0700

    Add besukma repository as submodule

--- besukma-main/prisma/migrations/20251109144732_add_category_relation_to_store/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Store" ADD COLUMN     "categoryId" INTEGER,
-ADD COLUMN     "establishedDate" TIMESTAMP(3),
-ADD COLUMN     "statusReason" TEXT,
-ADD COLUMN     "website" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Store" ADD CONSTRAINT "Store_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- besukma-main/prisma/migrations/20251113142225_update_order/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `description` on the `OrderStatusHistory` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "OrderStatusHistory" DROP COLUMN "description";
